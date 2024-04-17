679341ef3eb101e4f1c10d908c00856dab3d046b
Ronaldo <ronaldomjunior05@gmail.com>
2024-04-17 15:29:59 -0300

    feat: implements the card componentization usuing composition pattern

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,40 +1,123 @@
 import { Minus, Plus, ShoppingCart } from 'phosphor-react'
-import coffeeImage from '../../assets/coffee.png'
+import {
+  ComponentProps,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState,
+} from 'react'
 
-export function Card() {
+type CardProps = PropsWithChildren<ComponentProps<'article'>>
+
+interface CardImageProps extends ComponentProps<'img'> {
+  src: string
+  alt: string
+}
+
+type CardTagProps = PropsWithChildren<ComponentProps<'li'>>
+
+interface CardTagsProps extends ComponentProps<'ul'> {
+  children:
+    | React.ReactElement<CardTagProps>
+    | React.ReactElement<CardTagProps>[]
+}
+
+interface CardTitleProps extends ComponentProps<'h1'> {}
+
+interface CardDescriptionProps extends ComponentProps<'p'> {}
+
+interface CardDetailsProps extends PropsWithChildren<ComponentProps<'div'>> {}
+
+interface CardPriceProps extends ComponentProps<'span'> {
+  value: string
+}
+
+interface CardContextType {
+  handleDecreaseAmount: () => void
+  handleIncreaseAmount: () => void
+  amount: number
+}
+
+const CardContext = createContext({} as CardContextType)
+
+export function Card({ children }: CardProps) {
+  const [amount, setAmount] = useState(3)
+
+  const handleDecreaseAmount = () => {
+    if (amount > 1) setAmount((prev) => prev - 1)
+  }
+
+  const handleIncreaseAmount = () => {
+    setAmount((prev) => prev + 1)
+  }
+
   return (
-    <article className="relative grid w-fit max-w-[256px] grid-rows-[20px_1fr] place-items-center gap-3 p-6 before:absolute before:-z-10 before:row-start-2 before:h-full before:w-full before:rounded-md before:rounded-bl-[36px] before:rounded-tr-[36px] before:bg-base-card before:content-['']">
-      <img className="row-span-2" src={coffeeImage} alt="" />
-      <ul className="flex flex-wrap gap-1">
-        <li className="rounded-full bg-brand-light px-2 py-1 text-[0.625rem] font-bold uppercase leading-snug text-brand-dark ">
-          Tradicional
-        </li>
-      </ul>
-      <h1 className="font-baloo text-xl font-bold">Expresso Tradicional</h1>
-      <p className="text-center text-sm text-base-label">
-        O tradicional café feito com água quente e grãos moídos
-      </p>
-      <div className="flex w-full justify-between gap-2">
-        <span className="self-center whitespace-nowrap font-baloo text-2xl font-extrabold before:font-roboto before:text-sm before:font-normal before:text-base-text before:content-['R$_']">
-          9,90
+    <CardContext.Provider
+      value={{ handleDecreaseAmount, handleIncreaseAmount, amount }}
+    >
+      <article className="relative grid w-fit max-w-[256px] grid-rows-[20px_1fr] place-items-center gap-3 p-6 before:absolute before:-z-10 before:row-start-2 before:h-full before:w-full before:rounded-md before:rounded-bl-[36px] before:rounded-tr-[36px] before:bg-base-card before:content-['']">
+        {children}
+      </article>
+    </CardContext.Provider>
+  )
+}
+
+export function CardImage({ src, alt, ...props }: CardImageProps) {
+  return <img className="row-span-2" src={src} alt={alt} {...props} />
+}
+
+export function CardTags({ children }: CardTagsProps) {
+  return <ul className="flex flex-wrap justify-center gap-1">{children}</ul>
+}
+
+export function CardTag({ children }: CardTagProps) {
+  return (
+    <li className="rounded-full bg-brand-light px-2 py-1 text-[0.625rem] font-bold uppercase leading-snug text-brand-dark ">
+      {children}
+    </li>
+  )
+}
+
+export function CardTitle({ children }: CardTitleProps) {
+  return <h1 className="font-baloo text-xl font-bold">{children}</h1>
+}
+
+export function CardDescription({ children }: CardDescriptionProps) {
+  return <p className="text-center text-sm text-base-label">{children}</p>
+}
+
+export function CardPrice({ value }: CardPriceProps) {
+  return (
+    <span className="self-center whitespace-nowrap font-baloo text-2xl font-extrabold before:font-roboto before:text-sm before:font-normal before:text-base-text before:content-['R$_']">
+      {value}
+    </span>
+  )
+}
+
+export function CardDetails({ children }: CardDetailsProps) {
+  return <div className="flex w-full justify-between gap-2">{children}</div>
+}
+
+export function CardControls() {
+  const { handleIncreaseAmount, handleDecreaseAmount, amount } =
+    useContext(CardContext)
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className="grid grid-cols-3 place-items-center items-center rounded-md bg-base-button p-[8.5px]">
+        <button className="text-accent-default" onClick={handleDecreaseAmount}>
+          <Minus size={16} />
+        </button>
+        <span className="grid h-5 place-content-center text-base text-base-title">
+          {amount}
         </span>
-        <div className="flex items-center gap-2">
-          <div className="grid grid-cols-3 place-items-center items-center rounded-md bg-base-button p-[8.5px]">
-            <button className="text-accent-default">
-              <Minus size={16} />
-            </button>
-            <span className="grid h-5 place-content-center text-base text-base-title">
-              1
-            </span>
-            <button className="text-accent-default">
-              <Plus size={16} />
-            </button>
-          </div>
-          <button className="rounded-md bg-accent-dark p-2">
-            <ShoppingCart className="text-white" size={22} weight="fill" />
-          </button>
-        </div>
+        <button className="text-accent-default" onClick={handleIncreaseAmount}>
+          <Plus size={16} />
+        </button>
       </div>
-    </article>
+      <button className="rounded-md bg-accent-dark p-2">
+        <ShoppingCart className="text-white" size={22} weight="fill" />
+      </button>
+    </div>
   )
 }
